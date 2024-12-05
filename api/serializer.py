9455e683a9f2d70295d6e05ec570deddec17cada
YAMITHSALC27 <salcedoyamith@gmail.com>
2024-12-05 09:31:38 -0500

    intento_encender_el_server

--- api/serializer.py
+++ api/serializer.py
@@ -1,8 +1,15 @@
 from rest_framework import serializers
 from .models import programmer
+from .models import student
 
 class programmerSerializer(serializers.ModelSerializer):
     class Meta:
         model = programmer
         #fields = ('fullname', 'nickname', 'age', 'is_active')
+        fields = '__all__'
+        
+class studentSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = student
+        #fields = ('Nombre', 'Apellido', 'Sexo', 'Num_ficha', 'Formacion', 'Fecha_ingreso', 'is_active')
         fields = '__all__'
--- api/views.py
+++ api/views.py
@@ -1,9 +1,15 @@
 from rest_framework import viewsets
 from .models import programmer
 from .serializer import programmerSerializer
+from .models import student
+from .serializer import studentSerializer
 
 # Create your views here.
 
 class programmerViewSet(viewsets.ModelViewSet):
     queryset = programmer.objects.all()
     serializer_class = programmerSerializer
+    
+class studentViewSet(viewsets.ModelViewSet):
+    queryset = student.objects.all()
+    serializer_class = studentSerializer
